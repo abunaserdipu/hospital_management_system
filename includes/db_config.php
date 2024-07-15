ea38bee23c03c7f22b79ed5a3810e45c6465be81
abunaserdipu <abunaserdipu0@gmail.com>
2024-07-15 13:39:02 +0600

    :chart_with_upwards_trend: update db_config file with infinity free credentials

--- includes/db_config.php
+++ includes/db_config.php
@@ -1,11 +1,7 @@
 <?php
-    define("HOST","localhost");
-    define("USER","root");
-    define("PASS","");
-    define("DB","hospital_management");
+    define("HOST","sql307.infinityfree.com");
+    define("USER","if0_36905477");
+    define("PASS","CdolH1uJhmX5");
+    define("DB","if0_36905477_isdb_hospital");
     $db = new mysqli(HOST,USER,PASS,DB);
-
-
-
-
 ?>
